9589b6b051fe887965850a46d5d478093fda8532
JeffOni <jeffpbohorquez@outlook.com>
2025-04-11 15:25:09 -0500

    Cambios en mangeOptions livewrire controller y vista

--- app/Livewire/Admin/Options/ManageOptions.php
+++ app/Livewire/Admin/Options/ManageOptions.php
@@ -10,9 +10,30 @@ class ManageOptions extends Component
 
     public $options;//para obtener los datos de las opciones
 
+    public $openModal = false;//para abrir el modal
+
+    public $newOption = [//para crear una nueva opción especificados en el modal con sus inputs
+        'name' => '',
+        'type' => 1,
+        'features' => [
+            [
+                'value' => '',
+                'description' => '',
+            ]
+        ],
+    ];
+
     public function mount()
     {
        $this->options = Option::with('features')->get(); //para obtener los datos de las opciones evitando el problema de la carga de datos n+1
+    }
+
+    public function addFeature()
+    {//para agregar una nueva opción al array de opciones
+        $this->newOption['features'][] = [
+            'value' => '',
+            'description' => '',
+        ];
     }
 
     public function render()
--- resources/views/livewire/admin/options/manage-options.blade.php
+++ resources/views/livewire/admin/options/manage-options.blade.php
@@ -3,8 +3,11 @@
     <section class="bg-white rounded-lg shadow-lg ">
 
         <header class="px-6 py-2 border-b border-gray-200">
+            <div class="flex justify-between">
+                <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
+                <x-button wire:click="$set('openModal', true)" name="Nuevo" />
+            </div>
 
-            <h1 class="text-lg font-semibold text-gray-700">Opciones</h1>
 
         </header>
 
@@ -44,9 +47,7 @@
                                     @break
 
                                     @default
-
                                 @endswitch
-
                             @endforeach
 
                         </div>
@@ -60,5 +61,84 @@
         </div>
 
     </section>
+
+    {{-- Modal Section --}}
+
+    <x-dialog-modal wire:model="openModal" title="Crear Nueva Opcion">
+        <x-slot name="content">
+
+            {{-- Nombres --}}
+            <div class="grid grid-cols-2 gap-6 mb-4">
+
+                <div>
+
+                    <x-label class="mb-1" value="{{ __('Nombre') }}" />
+                    <x-input class="w-full" wire:model.defer="newOption.name" placeholder="Tamano , color" />
+                </div>
+
+                <div>
+                    <x-label class="mb-1" value="{{ __('Tipo') }}" />
+                    <x-select wire:model.defer="newOption.type" class="w-full">
+                        <option value="" disabled>Seleccione un tipo</option>
+                        {{-- 1 = Texto, 2 = Color --}}
+                        <option value="1">Texto</option>
+                        <option value="2">Color</option>
+                    </x-select>
+                </div>
+
+            </div>
+            {{-- Fin Nombres --}}
+
+            {{-- Valores --}}
+            <div class="flex items-center mb-4">
+
+                <hr class="flex-1 border-gray-300">
+
+                <span class="mx-4">Valores</span>
+
+                <hr class="flex-1 border-gray-300">
+
+            </div>
+
+            {{-- input de valores --}}
+            <div class="mb-4 space-y-4">
+
+                @foreach ($newOption['features'] as $index => $feature)
+                    <div class="p-6 border border-gray-200 rounded-lg">
+
+                        <div class="grid grid-cols-2 gap-6 mb-4">
+
+                            <div>
+                                <x-label class="mb-1" value="{{ __('Valor') }}" />
+                                <x-input class="w-full" placeholder="Ingrese el valor de la opcion"
+                                    wire:model.defer="newOption.features.{{ $index }}.value" />
+                            </div>
+
+                            <div>
+                                <x-label class="mb-1" value="{{ __('Descripción') }}" />
+                                <x-input class="w-full" placeholder="Ingrese una descripcion"
+                                    wire:model.defer="newOption.features.{{ $index }}.description" />
+                            </div>
+
+                        </div>
+
+                        <div class="flex justify-end">
+                            <x-button wire:click="addFeature" name="Agregar Valor" />
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+
+            {{-- Button feature --}}
+            <div class="flex justify-end">
+                <x-button wire:click="addFeature" name="Agregar Valor" />
+            </div>
+
+        </x-slot>
+        <x-slot name="footer"></x-slot>
+    </x-dialog-modal>
+
+    {{-- Fin Modal Section --}}
 
 </div>
