d42e23a818c2086999c6d2c2243640624f496421
JeffOni <jeffpbohorquez@outlook.com>
2025-05-12 14:20:56 -0500

    covers-list 50/50

--- resources/views/admin/covers/index.blade.php
+++ resources/views/admin/covers/index.blade.php
@@ -12,12 +12,16 @@
         <x-link href="{{ route('admin.covers.create') }}" type="primary" name="Nueva Portada" />
     </x-slot>
 
-    <ul class="space-y-4">
+    <ul class="space-y-4" id="covers-list">
+        {{-- Sección de encabezado --}}
+        {{-- Aquí se puede agregar un encabezado o título para la lista de portadas --}}
         {{-- Itera sobre cada portada y muestra su información --}}
         {{-- Cada portada se representa como un elemento de lista --}}
         {{-- Se utiliza el componente "cover-card" para mostrar la información de la portada --}}
         @foreach ($covers as $cover)
-            <li class="overflow-hidden bg-white rounded-lg shadow-md lg:flex">
+            <li class="overflow-hidden bg-white rounded-lg shadow-md lg:flex"
+            data-id="{{ $cover->id }}"
+            >
 
                 <img src="{{ $cover->image }}" class="lg:w-52 w-full aspect-[3/1] object-cover object-center "
                     alt="Imagen de Portada">
@@ -52,5 +56,34 @@
         @endforeach
     </ul>
 
+    <style>
+        .bg-blue-200 {
+            background-color: #bfdbfe !important;
+        }
+    </style>
+
+    @push('js')
+        {{-- script de libreria sortable --}}
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.6/Sortable.min.js"></script>
+        {{-- script de funcion sortable --}}
+        <script>
+            new Sortable(document.getElementById('covers-list'), {
+                animation: 150,
+                ghostClass: 'bg-blue-200',
+                store: {
+                    // Guardar el orden de los elementos en el almacenamiento local
+                    set: function(sortable) {
+                        const order = sortable.toArray();
+                        localStorage.setItem('covers-order', order.join(','));
+                    },
+                    // Recuperar el orden de los elementos del almacenamiento local
+                    get: function(sortable) {
+                        const order = localStorage.getItem('covers-order');
+                        return order ? order.split(',') : [];
+                    }
+                },
+            });
+        </script>
+    @endpush
 
 </x-admin-layout>
