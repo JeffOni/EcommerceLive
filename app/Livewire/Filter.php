83d787e1f7ebfc661c896562ec14daeffa49ccd9
JeffOni <jeffpbohorquez@outlook.com>
2025-05-21 15:44:24 -0500

    filters 65%

--- app/Livewire/Filter.php
+++ app/Livewire/Filter.php
@@ -3,15 +3,24 @@
 namespace App\Livewire;
 
 use App\Models\Option;
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Filter extends Component
 {
+    use WithPagination;
+    // Propiedad pública para almacenar el ID de la familia seleccionada
     // ID de la familia seleccionada, se utiliza para filtrar las opciones
     public $family_id;
 
     // Colección de opciones filtradas según la familia
     public $options;
+
+    public $selected_features = [];
+    // Propiedad pública para almacenar las características seleccionadas
+
+    public $orderBy = 1;
 
     // Método que se ejecuta al montar el componente
     public function mount()
@@ -28,12 +37,32 @@ class Filter extends Component
                         $query->where('family_id', $this->family_id);
                     });
                 },
-            ])->get();
+            ])->get()->toArray();
     }
 
     // Renderiza la vista asociada al componente Livewire
     public function render()
     {
-        return view('livewire.filter');
+        $products = Product::whereHas('subcategory.category', function ($query) {
+            $query->where('family_id', $this->family_id);
+        })
+            ->when($this->orderBy == 1, function ($query) {
+                $query->orderBy('created_at', 'desc');
+            })
+            ->when($this->orderBy == 2, function ($query) {
+                $query->orderBy('price', 'desc');
+            })
+            ->when($this->orderBy == 3, function ($query) {
+                $query->orderBy('price', 'asc');
+            })
+            ->when($this->selected_features, function ($query) {
+                $query->whereHas('variants.features', function ($query) {
+                    $query->whereIn('features.id', $this->selected_features);
+                });
+            })
+            ->paginate(12);
+        // Devuelve la vista 'livewire.filter' y pasa las opciones filtradas
+
+        return view('livewire.filter', compact('products'));
     }
 }
--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -1,50 +1,99 @@
 <div class="py-12 bg-white rounded-lg shadow-md">
     {{-- Componente de filtro de opciones y características --}}
-    <x-container class="flex px-4">
-        <aside class="flex-shrink-0 mr-8 w-52">
-            <ul class="space-y-4">
-                @foreach ($options as $option)
-                    <li class="mb-2" x-data="{ open: false }"> {{-- x-data: Inicializa el estado local de Alpine.js para este elemento, aquí 'open' controla si el panel está expandido o no --}}
-                        <button
-                            x-on:click="open = !open" {{-- x-on:click: Escucha el evento click y alterna el valor de 'open' (abre/cierra el panel) --}}
-                            class="flex items-center justify-between w-full px-4 py-2 text-left text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-blue-500">
-                            <span>{{ $option->name }}</span>
-                            <i class="ml-2 fas fa-chevron-down"
-                                x-bind:class="{'rotate-180': open, 'rotate-0': !open}" {{-- x-bind:class: Cambia la clase del ícono según el estado de 'open' para rotar la flecha --}}
-                                x-transition:enter="transition ease-out duration-300" {{-- x-transition:enter: Define la animación al mostrar el panel --}}
+    <x-container class="px-4 md:flex">
+        {{-- Filtro de opciones --}}
+        @if (count($options))
+
+            <aside class="mb-8 md:mr-8 md:flex-shrink-0 md:w-52 md:mb-0">
+                <ul class="space-y-4">
+                    @foreach ($options as $option)
+                        <li class="mb-2" x-data="{ open: false }"> {{-- x-data: Inicializa el estado local de Alpine.js para este elemento, aquí 'open' controla si el panel está expandido o no --}}
+                            <button x-on:click="open = !open" {{-- x-on:click: Escucha el evento click y alterna el valor de 'open' (abre/cierra el panel) --}}
+                                class="flex items-center justify-between w-full px-4 py-2 text-left text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus:ring focus:ring-blue-500">
+                                <span>{{ $option['name'] }}</span>
+                                <i class="ml-2 fas fa-chevron-down"
+                                    x-bind:class="{ 'rotate-180': open, 'rotate-0': !open }" {{-- x-bind:class: Cambia la clase del ícono según el estado de 'open' para rotar la flecha --}}
+                                    x-transition:enter="transition ease-out duration-300" {{-- x-transition:enter: Define la animación al mostrar el panel --}}
+                                    x-transition:enter-start="opacity-0 transform -translate-y-2"
+                                    x-transition:enter-end="opacity-100 transform translate-y-0"
+                                    x-transition:leave="transition ease-in duration-200" {{-- x-transition:leave: Define la animación al ocultar el panel --}}
+                                    x-transition:leave-start="opacity-100 transform translate-y-0"
+                                    x-transition:leave-end="opacity-0 transform -translate-y-2"></i>
+                            </button>
+                            <ul class="mt-2 space-y-2" x-show="open" {{-- x-show: Muestra u oculta este elemento según el valor de 'open' --}}
+                                x-transition:enter="transition ease-out duration-300"
                                 x-transition:enter-start="opacity-0 transform -translate-y-2"
                                 x-transition:enter-end="opacity-100 transform translate-y-0"
-                                x-transition:leave="transition ease-in duration-200" {{-- x-transition:leave: Define la animación al ocultar el panel --}}
+                                x-transition:leave="transition ease-in duration-200"
                                 x-transition:leave-start="opacity-100 transform translate-y-0"
-                                x-transition:leave-end="opacity-0 transform -translate-y-2"
-                            ></i>
-                        </button>
-                        <ul
-                            class="mt-2 space-y-2"
-                            x-show="open" {{-- x-show: Muestra u oculta este elemento según el valor de 'open' --}}
-                            x-transition:enter="transition ease-out duration-300"
-                            x-transition:enter-start="opacity-0 transform -translate-y-2"
-                            x-transition:enter-end="opacity-100 transform translate-y-0"
-                            x-transition:leave="transition ease-in duration-200"
-                            x-transition:leave-start="opacity-100 transform translate-y-0"
-                            x-transition:leave-end="opacity-0 transform -translate-y-2"
-                        >
-                            @foreach ($option->features as $feature)
-                                <li class="ml-4">
-                                    <label class="inline-flex items-center text-sm text-gray-700">
-                                        <x-checkbox class="mr-2"/>
-                                        {{ $feature->description }}
-                                    </label>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </li>
+                                x-transition:leave-end="opacity-0 transform -translate-y-2">
+                                @foreach ($option['features'] as $feature)
+                                    <li class="ml-4">
+                                        <label class="inline-flex items-center text-sm text-gray-700">
+                                            <x-checkbox value="{{ $feature['id'] }}" wire:model.live="selected_features"
+                                                class="mr-2" />
+                                            {{ $feature['description'] }}
+                                        </label>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endforeach
+                </ul>
+            </aside>
+
+        @endif
+
+
+        {{-- Contenido principal --}}
+        <div class="md:flex-1">
+
+            <div>
+                <span class="mr-2 text-sm font-semibold text-gray-700">
+                    Ordenar por:
+                </span>
+                <x-select wire:model.live="orderBy">
+                    <option value="1">
+                        Sellecionar
+                    </option>
+                    <option value="2">
+                        Precio : Mayor a Menor
+                    </option>
+                    <option value="3">
+                        Precio : Menor a Mayor
+                    </option>
+                </x-select>
+            </div>
+
+            <hr class="my-4 border-gray-300">
+
+            <div
+                class="grid grid-cols-1 gap-6 transition-all duration-300 ease-out sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                @foreach ($products as $product)
+                    <article class="flex flex-col h-full overflow-hidden bg-white rounded-md shadow-md">
+                        <div class="relative">
+                            <img src="{{ asset($product->image) }}" alt="Product Image"
+                                class="object-center w-full h-48">
+                            <div class="absolute top-0 left-0 px-2 py-1 text-white bg-blue-500 rounded-br-md">
+                                {{ $product->name }}
+                            </div>
+                        </div>
+                        <div class="flex flex-col flex-1 p-4">
+                            <h2 class="text-lg font-semibold line-clamp-2 min-h-[56px] text-gray-800">
+                                {{ $product->name }}</h2>
+                            {{-- <p class="mt-2 text-gray-600">{{ $product->description }}</p> --}}
+                            <p class="mt-4 text-xl font-bold text-gray-800">${{ $product->price }}</p>
+                            <x-link class="block w-full mt-auto text-center" name="Ver Mas"></x-link>
+                        </div>
+                    </article>
                 @endforeach
-            </ul>
-        </aside>
+            </div>
 
-        <div class="flex-1">
+            {{-- Paginación --}}
 
+            <div class="mt-8">
+                {{ $products->links() }} {{-- Renderiza los enlaces de paginación --}}
+            </div>
         </div>
     </x-container>
 </div>
